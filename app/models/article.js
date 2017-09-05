8024bd9de1a3fdafcb6124a58d09a0f87764324e
Josh Cole <saikojosh@users.noreply.github.com>
2017-09-05 15:09:11 +0100

    Fix - article model property names

--- app/models/article.js
+++ app/models/article.js
@@ -8,8 +8,8 @@ module.exports = function (Schema, Property) {
 
 	return new Schema(`Article`, {
 		feedId: new Property(`string`),
-		storyId: new Property(`string`),
-		storyUrl: new Property(`string`),
+		articleId: new Property(`string`),
+		articleUrl: new Property(`string`),
 		imageUrl: new Property(`string`),
 		title: new Property(`string`),
 		description: new Property(`string`),
