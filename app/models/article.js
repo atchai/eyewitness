1c5154f7f15443cd7563f1b93b28dcb75ab595ce
Josh Cole <saikojosh@users.noreply.github.com>
2017-09-06 18:09:55 +0100

    Add date fields to article model

--- app/models/article.js
+++ app/models/article.js
@@ -10,11 +10,13 @@ module.exports = function (Schema, Property, Reference) {
 		feedId: new Property(`string`),
 		articleId: new Property(`string`),
 		articleUrl: new Property(`string`),
+		articleDate: new Property(`date`),
 		imageUrl: new Property(`string`),
 		title: new Property(`string`),
 		description: new Property(`string`),
 		_receivedByUsers: [new Reference(`User`)],
 		_readByUsers: [new Reference(`User`)],
+		ingestedDate: new Property(`date`, Date.now),
 	});
 
 };
