4f331bce54526e45502276c3073bf3aed4502e86
Josh Cole <saikojosh@users.noreply.github.com>
2017-09-06 14:54:26 +0100

    Fix - articleId property name in models

--- app/models/received.js
+++ app/models/received.js
@@ -9,7 +9,7 @@ module.exports = function (Schema, Property, Reference) {
 	return new Schema(`Received`, {
 		userId: new Reference(`User`),
 		feedId: new Property(`string`),
-		storyId: new Property(`string`),
+		articleId: new Property(`string`),
 		timestamp: new Property(`date`, Date.now),
 	});
 
