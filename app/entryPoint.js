792c39566bfc68d2b333dc8c024334db13316972
Josh Cole <saikojosh@users.noreply.github.com>
2017-09-07 19:29:59 +0100

    Add entry point file to load either app or read server

---
+++ app/entryPoint.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/*
+ * ENTRY POINT
+ * Loads either the app or the read server depending on environment config.
+ */
+
+require(process.env.ENTRY_POINT === `read-server` ? `./modules/read-server` : `./app`);
