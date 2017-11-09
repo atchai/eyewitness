6fbcc92311e320a24840f8b3e28d597e9a5d6d7c
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-09 15:22:01 +0000

    Refactor entrypoint to support exporting some methods

--- app/entryPoint.js
+++ app/entryPoint.js
@@ -5,7 +5,21 @@
  * Loads either the bot or the read server depending on environment config.
  */
 
-require(process.env.ENTRY_POINT === `read-server` ? `./readServer` : `./bot`);
+/* eslint-disable global-require */
+
 // Ensure we always work relative to this script.
 process.chdir(__dirname);
 
+// Include the appropriate entry point.
+let entryPointFilename;
+
+switch (process.env.ENTRY_POINT) {
+	case `read-server`: entryPointFilename = `./readServer`; break;
+	case `bot`: entryPointFilename = `./bot`; break;
+	default: entryPointFilename = null; break;
+}
+
+if (entryPointFilename) { require(entryPointFilename); }
+
+// Expose some functions to the outside world.
+module.exports = require(`./modules/mainExportables`);
