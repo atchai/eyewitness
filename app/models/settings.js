9dfa575a556453363f6f449f5b32eb13fb63145e
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-17 15:08:10 +0000

    Rename is bot enabled property for consistency with article model

--- app/models/settings.js
+++ app/models/settings.js
@@ -7,7 +7,7 @@
 module.exports = function (Schema, Property/* , Reference */) {
 
 	return new Schema(`Settings`, {
-		botEnabled: new Property(`boolean`, true),
+		isBotEnabled: new Property(`boolean`, true),
 		welcomeMessages: [new Property(`string`)],
 	});
 
