c0d74a1383f63f6f9617d490b1b618d45763ef33
Justin Emery <justinemery@gmail.com>
2020-01-23 19:32:50 +0700

    Fix renew script

--- nginx/certs_renew.sh
+++ nginx/certs_renew.sh
@@ -6,7 +6,7 @@ CERTBOT_OPTION=$1
 # Renew all certificates (if necessary)
 certbot renew $CERTBOT_OPTION
 # Reload nginx without downtime
-docker exec eyewitness-bot_nginx_bot_1 nginx -s reload
-docker exec eyewitness-bot_nginx_read-server_1 nginx -s reload
+docker exec eyewitness_nginx_bot_1 nginx -s reload
+docker exec eyewitness_nginx_read-server_1 nginx -s reload
 docker exec eyewitness-ui_nginx_ui_1 nginx -s reload
 
